8645a0571727cf9bd2624d4a78dd4d31dc85e1ee
Mike Knepper <michaelrknepper@gmail.com>
2015-03-07 17:34:31 -0600

    Adds magic dependencies for rbenv, and removes mysql root password

--- script.sh
+++ script.sh
@@ -9,10 +9,11 @@ VM_HOME=$PWD dotfiles/bin/vm.sh&
 git clone https://github.com/sstephenson/rbenv.git .rbenv
 git clone https://github.com/sstephenson/ruby-build.git .ruby-build
 .ruby-build/install.sh
-apt-get install -y libssl-dev
+apt-get install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev nodejs
 
 echo "mysql-server mysql-server/root_password password root" | debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password root" | debconf-set-selections
-apt-get -y install mysql-server
+apt-get install -y mysql-server libmysqlclient-dev
+mysqladmin -u root -proot password ''
 
 chown -R vagrant:vagrant .
