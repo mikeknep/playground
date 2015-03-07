359a545dbf5435071193206a6985aef615247ca2
Mike Knepper <michaelrknepper@gmail.com>
2015-03-07 15:31:20 -0600

    Provisions rbenv and mysql

--- script.sh
+++ script.sh
@@ -4,5 +4,15 @@ apt-get update
 apt-get install -y git
 
 git clone https://github.com/mikeknep/dotfiles.git
-VM_HOME="/home/vagrant" ./dotfiles/bin/vm.sh
-chown vagrant:vagrant -R dotfiles/
+VM_HOME=$PWD dotfiles/bin/vm.sh&
+
+git clone https://github.com/sstephenson/rbenv.git .rbenv
+git clone https://github.com/sstephenson/ruby-build.git .ruby-build
+.ruby-build/install.sh
+apt-get install -y libssl-dev
+
+echo "mysql-server mysql-server/root_password password root" | debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password root" | debconf-set-selections
+apt-get -y install mysql-server
+
+chown -R vagrant:vagrant .
