d21ba26e55e07cb7476bf186f9a8493e07996d5b
Mike Knepper <michaelrknepper@gmail.com>
2015-03-08 21:28:13 -0500

    Adds vundle plugin install to shell provision script

--- README.md
+++ README.md
@@ -2,3 +2,19 @@
 
 A virtual machine for development.
 Currently very much a work in progress.
+
+## Status
+
+### Shell provisioner
+**What it does**
+- symlinks dotfiles
+- installs vundle plugins
+- installs rbenv and ruby-build
+- installs mysql with password-less root login
+
+**Steps to start working on a Rails app**
+- `git clone <github repo>`
+- `rbenv install <version of Ruby used by app>`
+- `gem install bundler`
+- `bundle install`
+- `rake db:create db:migrate db:test:prepare`
--- script.sh
+++ script.sh
@@ -4,7 +4,11 @@ apt-get update
 apt-get install -y git
 
 git clone https://github.com/mikeknep/dotfiles.git
-VM_HOME=$PWD dotfiles/bin/vm.sh&
+VM_HOME=$PWD dotfiles/bin/vm.sh
+
+git clone https://github.com/gmarik/Vundle.vim.git .vim/bundle/Vundle.vim
+echo "Installing vundle plugins..."
+su vagrant -c "sudo vim -u ~/dotfiles/vim/bundles.vim +PluginInstall +qall -e -s"
 
 git clone https://github.com/sstephenson/rbenv.git .rbenv
 git clone https://github.com/sstephenson/ruby-build.git .ruby-build
