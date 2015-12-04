f7ef6c4916ef1d62ef70fe0305c395a2602f60ac
Mike Knepper <michaelrknepper@gmail.com>
2015-12-03 22:19:44 -0600

    Always update apt-get; minor renames; quieter

--- installers.sh
+++ installers.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+update_apt_get() {
+  echo Updating apt-get
+  apt-get -qq update
+}
+
 install_git() {
   if ! type git > /dev/null 2>&1; then
-    apt-get update
-    apt-get install -y git
+    echo Installing git
+    apt-get -qqy install git
   fi
 }
 
@@ -17,25 +22,27 @@ install_dotfiles() {
 install_vundle() {
   if [ ! -e .vim/bundle/Vundle.vim ]; then
     git clone https://github.com/gmarik/Vundle.vim.git .vim/bundle/Vundle.vim
-    echo "Installing vundle plugins. (This could take a while...)"
+    echo "Installing vundle plugins (this could take a while)"
     su vagrant -c "sudo vim -u ~/dotfiles/vim/bundles.vim +PluginInstall +qall -e -s"
   fi
 }
 
-install_ruby() {
-  if ! type ruby > /dev/null 2>&1; then
+install_rbenv() {
+  if ! type rbenv > /dev/null 2>&1; then
     git clone https://github.com/sstephenson/rbenv.git .rbenv
     git clone https://github.com/sstephenson/ruby-build.git .ruby-build
+    echo Installing ruby-build
     .ruby-build/install.sh
-    apt-get install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev nodejs
+    apt-get -qqy install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev nodejs
   fi
 }
 
 install_mysql() {
   if ! type mysql > /dev/null 2>&1; then
+    echo Installing mysql
     echo "mysql-server mysql-server/root_password password root" | debconf-set-selections
     echo "mysql-server mysql-server/root_password_again password root" | debconf-set-selections
-    apt-get install -y mysql-server libmysqlclient-dev
+    apt-get -qqy install -y mysql-server libmysqlclient-dev
     mysqladmin -u root -proot password ''
   fi
 }
--- provision.sh
+++ provision.sh
@@ -2,9 +2,10 @@
 
 source ./host/installers.sh
 
+update_apt_get
 install_git
 install_dotfiles
 install_vundle
-install_ruby
+install_rbenv
 install_mysql
 grant_permissions
